f0d81d8b0e16f73c405b410f48ef76036e926507
agilcalderon <agil@webcreek.com>
2019-11-11 00:14:15 -0500

    finish test

--- src/app/fav-item/fav-item.component.ts
+++ src/app/fav-item/fav-item.component.ts
@@ -10,12 +10,19 @@ export class FavItemComponent implements OnInit {
 
   @Input() item:Movie;
   constructor() { }
+  id=this.uuidv4();
 
   @Output() removeItem=new EventEmitter<number>();
   ngOnInit() {
-
+    
   }
   remove(id:number){
     this.removeItem.emit(id);
+  }
+  uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
   }
 }
